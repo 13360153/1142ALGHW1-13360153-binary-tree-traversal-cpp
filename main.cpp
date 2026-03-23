8c0a12e5ccef4f7ca711e26ec2125c6cbec5e404
13360153 <13360153@me.mcu.edu.tw>
2026-03-23 08:50:47 +0000

    done

--- main.cpp
+++ main.cpp
@@ -15,14 +15,32 @@ struct Node {
 
 void preorder(Node* root) {
     //TODO
+    if(root == NULL) 
+        return;
+    cout << root -> data << " ";
+    preorder(root -> left);
+    preorder(root -> right);
+    
 }
 
 void inorder(Node* root) {
-    //TODO
+    if(root == NULL) 
+        return;
+    
+    inorder(root -> left);
+    cout << root -> data << " ";
+    inorder(root -> right);
+    
 }
 
 void postorder(Node* root) {
-    //TODO
+    if(root == NULL) 
+        return;
+    
+    postorder(root -> left);
+    postorder(root -> right);
+    cout << root -> data << " ";
+    
 }
 
 void deleteTree(Node* root) {
